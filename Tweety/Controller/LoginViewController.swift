d9b0168a8b35b0dac04aa62fa7da558cd87027ac
Gizem Genc <gencgizem97@gmail.com>
2019-09-10 12:45:04 +0300

    Alamofire kurulumu yapıldı

---
+++ Tweety/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tweety
+//
+//  Created by Büşranur Yetişmiş on 3.09.2019.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
